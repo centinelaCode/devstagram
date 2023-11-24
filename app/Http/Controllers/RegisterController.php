199918b9286e2bf10bd67cebfafa9a28f67a9620
centinelaCode <centinela1024tb@gmail.com>
2023-11-24 14:14:39 -0600

    [ADD] se usa eloquent y el model para crear un registro en users

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -26,6 +27,15 @@ class RegisterController extends Controller
          'password' => ['required', 'confirmed', 'min:6', 'max:30'],
       ]);
 
-      dd('Creando Usuario');
+      // dd('Creando Usuario');
+
+      // creando un registro
+      User::create([
+         'name' => $request->name,
+         'username' => $request->username,
+         'email' => $request->email,
+         'password' => $request->password
+      ]);
+
    }
 }
